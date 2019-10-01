d42909b785e533115ef906896283f42ba5496461
Honour <onurpisirir@hotmail.com>
2019-10-01 19:26:04 +0300

    İlk C projemiz

---
+++ MerhabaDunya/MerhabaDunya/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MerhabaDunya
+//
+//  Created by Onur Pisirir on 1.10.2019.
+//  Copyright © 2019 Onur Pisirir. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}

84a3e0049ef8dc79c35bca6eadc040b8ef999e66
Honour <onurpisirir@hotmail.com>
2019-10-09 21:46:26 +0300

    dışarıdan veri alan ilk örnek

---
+++ Selamlama/Selamlama/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  Selamlama
+//
+//  Created by Onur Pisirir on 9.10.2019.
+//  Copyright © 2019 Onur Pisirir. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    string isim;
+    
+    cout<<"isminizi giriniz:";
+    cin>>isim;
+    
+    cout<<"Merhaba "<<isim;
+    
+    cout<<"Program sonlanmıştır.";
+    
+    
+    
+    return 0;
+}
